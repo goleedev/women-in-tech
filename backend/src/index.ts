15507dc10278cd319045b77769690e42e557d300
goleee <golee.dev@gmail.com>
2025-02-26 23:38:53 +0000

    refactor: update users api for CRUD

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,18 +3,16 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import userRoutes from './routes/users';
-import eventRoutes from './routes/events';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8008;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-app.use('/api/events', eventRoutes);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
